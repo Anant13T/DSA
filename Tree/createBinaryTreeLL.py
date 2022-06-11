8c1823b168aaee3d9288a1ceee524213a26ddf24
Anant13T <ananttayal13@gmail.com>
2022-06-11 14:09:28 +0530

    Create createBinaryTreeLL.py

---
+++ Tree/createBinaryTreeLL.py
@@ -0,0 +1,21 @@
+class Treenode:
+    def __init__(self,data):
+        self.data=data
+        self.leftchild=None
+        self.rightchild=None
+
+new1=Treenode('drinks')
+lchild=Treenode("cold")
+rchild=Treenode("hot")
+new1.leftchild=lchild
+new1.rightchild=rchild
+
+def preorderTraversal(rootnode):
+    if not rootnode:
+        return 
+    else:
+        print(rootnode.data)
+        preorderTraversal(rootnode.leftchild)
+        preorderTraversal(rootnode.rightchild)
+
+preorderTraversal(new1)
