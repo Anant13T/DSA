3ad08883ccdf4d3e083ed7525d5526e608f7a72c
Anant13T <ananttayal13@gmail.com>
2022-06-14 23:29:00 +0530

    Update BST.py

--- Tree/BST.py
+++ Tree/BST.py
@@ -1,3 +1,6 @@
+from numpy import insert
+
+
 class BSTNode:
     def __init__(self,data):
         self.data=data
@@ -5,6 +8,19 @@ class BSTNode:
         self.rightchild=None
 
     def insertNode(self,rootNode,nodevalue):
-        pass
+        if rootNode.data==None:
+            rootNode.data=nodevalue
+        elif nodevalue<=rootNode.data:
+            if rootNode.leftchild==None:
+                rootNode.leftchild=BSTNode(nodevalue)
+            else:
+                insert(rootNode.leftchild.nodevalue)
+        else:
+            if rootNode.rightchild==None:
+                rootNode.rightchild=BSTNode(nodevalue)
+            else:
+                insert(rootNode.rightchild.nodevalue)
 
 n1=BSTNode(None)
+n1.insertNode(n1,70)
+n1.insertNode(n1,25)
