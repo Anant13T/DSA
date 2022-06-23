f6ef8f24324125a1ec8bb7be35dbd8e1c79a8d84
Anant13T <ananttayal13@gmail.com>
2022-06-23 19:54:44 +0530

    Update trie.py

--- Trie/trie.py
+++ Trie/trie.py
@@ -7,4 +7,17 @@ class Trie:
     def __init__(self):
         self.root=TrieNode()
 
+    def insertString(self,word):
+        current=self.root
+        for i in word:
+            ch=i
+            node=current.children.get(ch)
+            if node==None:
+                node=TrieNode()
+                current.children.update({ch:node})
+            current=node
+        current.endOfString=True
+
 n1=Trie()
+n1.insertString("app")
+n1.insertString("appl")
