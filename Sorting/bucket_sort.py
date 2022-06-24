b709a23422ecc5eee738f4c406f263b1d7c5a454
Anant13T <ananttayal13@gmail.com>
2022-06-25 00:01:34 +0530

    Update bucket_sort.py

--- Sorting/bucket_sort.py
+++ Sorting/bucket_sort.py
@@ -1,5 +1,15 @@
 import math
 
+def insertionSort(customList):
+    for i in range(1,len(customList)):
+        key=customList[i]
+        j=i-1
+        while j>=0 and key<customList[j]:
+            customList[j+1]=customList[j]
+            j-=1
+        customList[j+1]=key
+    return customList
+
 def bucketSort(customList):
     noBucket=round(math.sqrt(len(customList)))
     maxValue=max(customList)
@@ -8,4 +18,15 @@ def bucketSort(customList):
         arr.append([])
     for j in customList:
         index_b=math.ceil(j*noBucket/maxValue)
-        
+        arr[index_b-1].append(j)
+    for i in range(noBucket):
+        arr[i]=insertionSort(arr[i])
+    k=0
+    for i in range(noBucket):
+        for j in range(len(arr[i])):
+            customList[k]=arr[i][j]
+            k+=1
+    print(customList)
+
+a1=[54,34,23,1,34,32,90]
+bucketSort(a1)
