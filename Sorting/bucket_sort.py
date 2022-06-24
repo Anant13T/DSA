2f9e51d8af8319b4342f9db0755b9592886b6f4d
Anant13T <ananttayal13@gmail.com>
2022-06-24 23:53:48 +0530

    Update bucket_sort.py

--- Sorting/bucket_sort.py
+++ Sorting/bucket_sort.py
@@ -2,4 +2,10 @@ import math
 
 def bucketSort(customList):
     noBucket=round(math.sqrt(len(customList)))
-    
+    maxValue=max(customList)
+    arr=[]
+    for i in range(noBucket):
+        arr.append([])
+    for j in customList:
+        index_b=math.ceil(j*noBucket/maxValue)
+        
