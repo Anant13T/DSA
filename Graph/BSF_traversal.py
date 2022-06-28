12147f90a8cba755258c80286155c9a3d5757b33
Anant13T <ananttayal13@gmail.com>
2022-06-28 19:44:14 +0530

    Update BSF_traversal.py

--- Graph/BSF_traversal.py
+++ Graph/BSF_traversal.py
@@ -7,6 +7,17 @@ class Graph:
     def addEdge(self,vertex,edge):
         self.gdict[vertex].append(edge)
 
+    def bfs(self,vertex):
+        visited=[vertex]
+        queue=[vertex]
+        while queue:
+            deVertex=queue.pop(0)
+            print(deVertex)
+            for adjacentVertex in self.gdict[deVertex]:
+                if adjacentVertex not in visited:
+                    visited.append(adjacentVertex)
+                    queue.append(adjacentVertex)
+
 customDict={
     'a':["b","c"],
     "b":["a",'d','e'],
@@ -17,4 +28,4 @@ customDict={
 }
 g1=Graph(customDict)
 g1.addEdge('e',"c")
-print(g1.gdict)
+g1.bfs("a")
