af6904c8886db22e700722f56f3d9157d3755102
Anant13T <ananttayal13@gmail.com>
2022-06-29 17:31:10 +0530

    -

--- Graph/BSF_SSSP.py
+++ Graph/BSF_SSSP.py
@@ -1,7 +1,32 @@
+import queue
+
+
 class Graph:
     def __init__(self,gdict=None):
         if gdict is None:
             gdict={}
         self.gdict=gdict
     
-    
+    def bfs(self,start,end):
+        queue=[]
+        queue.append([start])
+        while queue:
+            path=queue.pop(0)
+            node=path[-1]
+            if node==end:
+                return path
+            for adjacent in self.gdict.get(node,[]):
+                new_path=list(path)
+                new_path.append(adjacent)
+                queue.append(new_path)
+
+customDict={
+    'a':["b","c"],
+    "b":["a",'d','e'],
+    "c":['a','e'],
+    "d":['b',"e","f"],
+    "e":["c","d","f"],
+    "f":["d","e"]
+}
+g1=Graph(customDict)
+print(g1.bfs('a',"e"))
