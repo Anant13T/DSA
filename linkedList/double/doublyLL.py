f2ebea9578a4697b66d0c2bc444a490d0e0be7bf
Anant13T <ananttayal13@gmail.com>
2022-08-25 10:34:54 +0530

    Update doublyLL.py

--- linkedList/double/doublyLL.py
+++ linkedList/double/doublyLL.py
@@ -16,3 +16,28 @@ class DLL:
             while temp:
                 print(temp.data)
                 temp=temp.next
+    
+    def Ib(self,data):
+        nb=Node(data)
+        temp=self.head
+        temp.prev=nb
+        nb.next=temp
+        self.head=nb
+    
+    def IE(self,data):
+        ne=Node(data)
+        temp=self.head
+        while temp.next is not None:
+            temp=temp.next
+        temp.next=ne
+        ne.prev=temp
+
+    def IP(self,data,pos):
+        np=Node(data)
+        temp=self.head
+        for i in range(1,pos-1):
+            temp=temp.next
+        np.prev=temp
+        np.next=temp.next
+        temp.next.prev=np
+        temp.next=np
